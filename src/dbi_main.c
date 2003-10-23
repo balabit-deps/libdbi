daef70bcbcc58413cc67532c0ea909c8d5ecee6c
Markus Hoenicka <markus.hoenicka@mhoenicka.de>
2003-10-23 17:57:50 +0000

    fixed segfault if driver directory contains files without suffix

--- src/dbi_main.c
+++ src/dbi_main.c
@@ -17,7 +17,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * 
- * $Id: dbi_main.c,v 1.51 2003/06/21 21:36:19 dap24 Exp $
+ * $Id: dbi_main.c,v 1.52 2003/10/23 17:57:50 mhoenicka Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -123,7 +123,7 @@ int dbi_initialize(const char *driverdir) {
 		while ((driver_dirent = readdir(dir)) != NULL) {
 			driver = NULL;
 			snprintf(fullpath, FILENAME_MAX, "%s/%s", effective_driverdir, driver_dirent->d_name);
-			if ((stat(fullpath, &statbuf) == 0) && S_ISREG(statbuf.st_mode) && (!strcmp(strrchr(driver_dirent->d_name, '.'), DRIVER_EXT))) {
+			if ((stat(fullpath, &statbuf) == 0) && S_ISREG(statbuf.st_mode) && strrchr(driver_dirent->d_name, '.') && (!strcmp(strrchr(driver_dirent->d_name, '.'), DRIVER_EXT))) {
 				/* file is a stat'able regular file that ends in .so (or appropriate dynamic library extension) */
 				driver = _get_driver(fullpath);
 				if (driver && (driver->functions->initialize(driver) != -1)) {
