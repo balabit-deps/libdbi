b5698d58295352f4774ef7f7c6f41e8762fcabb8
Markus Hoenicka <markus.hoenicka@mhoenicka.de>
2005-08-27 23:29:02 +0000

    Peter's OSX patches

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 # autogen.sh - generates configure using the autotools
-# $Id: autogen.sh,v 1.2 2003/07/09 07:24:52 dap24 Exp $
-libtoolize --force --copy
-aclocal
-autoheader
-automake --add-missing
+# $Id: autogen.sh,v 1.3 2005/08/27 23:29:02 mhoenicka Exp $
+: ${LIBTOOLIZE=libtoolize}
+: ${ACLOCAL=aclocal}
+: ${AUTOHEADER=autoheader}
+: ${AUTOMAKE=automake}
+: ${AUTOCONF=autoconf}
+
+
+${LIBTOOLIZE} --force --copy
+${ACLOCAL}
+${AUTOHEADER}
+${AUTOMAKE} --add-missing
 
 echo
-echo "Using `autoconf --version`, if you have problems"
-echo "while building then edit autogen.sh and rerun it..."
+echo "Using `${AUTOCONF} --version`, if you have problems"
+echo "while building then set the environemnt variable AUTOCONF"
+echo "to a different autoconf version and rerun $0"
 echo
-autoconf
+${AUTOCONF}
 
 ## autoconf 2.53 will not work, at least on FreeBSD. Change the following
 ## line appropriately to call autoconf 2.13 instead. This one works for
--- src/dbi_main.c
+++ src/dbi_main.c
@@ -17,7 +17,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * 
- * $Id: dbi_main.c,v 1.72 2005/08/25 20:26:25 mhoenicka Exp $
+ * $Id: dbi_main.c,v 1.73 2005/08/27 23:29:03 mhoenicka Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -1333,7 +1333,7 @@ static void * dyld_dlopen(const char * file)
 }
 static void * dyld_dlsym(void * hand, const char * name)
 {
-        NSSymbol * sym=NSLookupSymbolInModule((NSModule)hand, name);
+        NSSymbol sym=NSLookupSymbolInModule((NSModule)hand, name);
 	void * addr = NULL;
 	dyld_error_set=0;
 	if (sym) addr=(void*)NSAddressOfSymbol(sym);
